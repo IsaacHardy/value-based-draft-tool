0d328e1854f9f2a2818268a502f2158c437a7495
Isaac Hardy <isaac@isaachardy.io>
2020-08-31 21:14:48 -0400

    headers for columns

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,6 +20,7 @@ const Home = () => {
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="py-2 text-center text-lg leading-5 font-medium text-white bg-gray-600">Overall</div>
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table class="min-w-full divide-y divide-gray-200">
                 <thead>
@@ -38,6 +39,7 @@ const Home = () => {
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="py-2 text-center text-lg leading-5 font-medium text-white bg-yellow-400">QB</div>
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table class="min-w-full divide-y divide-gray-200">
                 <thead>
@@ -56,6 +58,7 @@ const Home = () => {
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="py-2 text-center text-lg leading-5 font-medium text-white bg-green-400">RB</div>
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table class="min-w-full divide-y divide-gray-200">
                 <thead>
@@ -74,6 +77,7 @@ const Home = () => {
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="py-2 text-center text-lg leading-5 font-medium text-white bg-red-400">WR</div>
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table class="min-w-full divide-y divide-gray-200">
                 <thead>
@@ -92,6 +96,7 @@ const Home = () => {
       <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="py-2 text-center text-lg leading-5 font-medium text-white bg-blue-400">TE</div>
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table class="min-w-full divide-y divide-gray-200">
                 <thead>
